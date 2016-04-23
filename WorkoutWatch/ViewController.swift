ef5709f2dda2f7b74281922cd97ffd2c9a8a9b35
Jonathan Wiley <wiley@lunarlincoln.com>
2016-04-23 11:25:10 -0500

    Initial Commit

---
+++ WorkoutWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkoutWatch
+//
+//  Created by Jonathan Wiley on 4/23/16.
+//  Copyright © 2016 Jonathan Wiley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
